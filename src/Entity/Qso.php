d1c214cfff58aa7e70ba5d89b45abd6f69a6bd9d
Bill Talkington <talkington.bill@gmail.com>
2026-05-17 00:58:20 -0400

    Add Qso Entity, Repository, and associated migration

---
+++ src/Entity/Qso.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\State;
+use App\Repository\QsoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QsoRepository::class)]
+class Qso
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $activatorCallsign = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $parkReference = null;
+
+    #[ORM\Column(enumType: State::class)]
+    private ?State $state = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $band = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mode = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $contactedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getActivatorCallsign(): ?string
+    {
+        return $this->activatorCallsign;
+    }
+
+    public function setActivatorCallsign(string $activatorCallsign): static
+    {
+        $this->activatorCallsign = $activatorCallsign;
+
+        return $this;
+    }
+
+    public function getParkReference(): ?string
+    {
+        return $this->parkReference;
+    }
+
+    public function setParkReference(string $parkReference): static
+    {
+        $this->parkReference = $parkReference;
+
+        return $this;
+    }
+
+    public function getState(): ?State
+    {
+        return $this->state;
+    }
+
+    public function setState(State $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getBand(): ?string
+    {
+        return $this->band;
+    }
+
+    public function setBand(string $band): static
+    {
+        $this->band = $band;
+
+        return $this;
+    }
+
+    public function getMode(): ?string
+    {
+        return $this->mode;
+    }
+
+    public function setMode(string $mode): static
+    {
+        $this->mode = $mode;
+
+        return $this;
+    }
+
+    public function getContactedAt(): ?\DateTime
+    {
+        return $this->contactedAt;
+    }
+
+    public function setContactedAt(?\DateTime $contactedAt): static
+    {
+        $this->contactedAt = $contactedAt;
+
+        return $this;
+    }
+}
---
+++ src/Repository/QsoRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Qso;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Qso>
+ */
+class QsoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Qso::class);
+    }
+
+//    /**
+//     * @return Qso[] Returns an array of Qso objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('q')
+//            ->andWhere('q.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('q.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Qso
+//    {
+//        return $this->createQueryBuilder('q')
+//            ->andWhere('q.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
